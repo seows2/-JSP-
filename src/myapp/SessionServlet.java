e42035c4c67ddf72c8872c9a74369f9aa87916e2
SeoWS <seows2@naver.com>
2020-04-17 18:24:42 +0900

    [4] HttpSession 실습

---
+++ src/myapp/SessionServlet.java
@@ -0,0 +1,58 @@
+package myapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/SessionServlet")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><BODY>");
+		out.println("Session test <br>");
+		
+		HttpSession session = request.getSession();
+		if(session.isNew()) {
+			out.println("This is a new Session");
+			session.setMaxInactiveInterval(30);
+		} else {
+			out.println("<br> create time = " + session.getCreationTime());
+			out.println("<br> last accessed time = " + session.getLastAccessedTime());
+			out.println("<br> maximun inactive interval = " + session.getMaxInactiveInterval());
+		}
+		out.println("</BODY></HTML>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
